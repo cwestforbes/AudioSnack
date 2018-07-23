293f5636dbe023836ac0d3c0fd668a112696845c
Cory West-Forbes <cwestforbes@gmail.com>
2018-07-23 14:05:50 -0700

    add record/pause toggle

--- src/app/Record.js
+++ src/app/Record.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import { View, ScrollView, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import { View, ScrollView, StyleSheet, Image, ImageBackground, Text, TouchableOpacity } from 'react-native';
 import Expo, {Permissions} from 'expo';
 
 export class Record extends Component {
   constructor() {
     super();
+    this.state = {
+      isRecording: false
+    }
   }
 
   static navigationOptions = {
     tabBarLabel: 'Record',
     tabBarIcon: ({ tintColor }) => <Image source={require('./../../public/img/addRecordIcon.png')} style={{ width: 27, height: 27 }} />
   };
+
+  componentDidMount() {
+    this.getRecordPermissions();
+  }
 
   getRecordPermissions = async () => {
     const { Permissions } = Expo;
@@ -29,9 +36,19 @@ export class Record extends Component {
         <Image source={require('./../../public/img/hold-waveform.png')} style={{ height: 200, width: 380 }} />
         <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 50, marginBottom: 30 }}>
           <Image source={require('./../../public/img/cancelBtn.png')} style={{ height: 70, width: 70 }} />
-          <TouchableOpacity onPress={() => {alert('clicked')}}>
-            <Image source={require('./../../public/img/recordBtn.png')} style={{ height: 90, width: 90 }} />
-          </TouchableOpacity>
+          {!this.state.isRecording ? (
+            <TouchableOpacity
+              onPress={() => {this.setState({
+                isRecording: true});
+                console.log(this.state);
+              }}>
+              <Image source={require('./../../public/img/recordBtn.png')} style={{ height: 90, width: 90 }} />
+            </TouchableOpacity>
+          ) : ( <TouchableOpacity onPress={() => {alert('changeClicked')}}>
+                  <ImageBackground source={require('./../../public/img/recordBtn.png')} style={{ height: 90, width: 90 }}>
+                    <Text>II</Text>
+                  </ImageBackground>
+                </TouchableOpacity>)}
           <Image source={require('./../../public/img/yesBtn.png')} style={{ height: 70, width: 70 }} />
         </View>
       </View>
