da38122d3b379258341f53e3948916c8ae81574a
Cory West-Forbes <cwestforbes@gmail.com>
2018-07-24 14:32:00 -0700

    finalize search content styling

--- src/app/Search.js
+++ src/app/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, TextInput, Text } from 'react-native';
+import { View, StyleSheet, Image, TextInput, Text, ScrollView } from 'react-native';
 import * as firebase from 'firebase';
 
 export class Search extends Component {
@@ -41,7 +41,7 @@ export class Search extends Component {
   render() {
     console.log(this.state)
     return (
-      <View>
+      <ScrollView>
         <View style={styles.iosHeader} />
         <View style={styles.searchHeader}>
           <View style={styles.searchContainer}>
@@ -64,19 +64,19 @@ export class Search extends Component {
           </View>
         </View>
         <View>
-          <View style={{ height: 30, width: 200, justifyContent: 'center'}}>
-            <Text>adfhasddafhas</Text>
-              {(this.state.search === null || this.state.search === '') ? null : this.state.searchResults
-                .filter(
-                 user =>`${user.username} ${user.email}`.toLowerCase().indexOf((this.state.search).toLowerCase()) >= 0)
-                .map(user => (
-                <View key={user.username} style={{marginTop: 60, height: 200}}>
-                  <Text>{user.username} {user.email}</Text>
+          <View style={{width: '100%'}}>
+            {(this.state.search === null || this.state.search === '') ? null : this.state.searchResults
+              .filter(
+               user =>`${user.username}`.toLowerCase().indexOf((this.state.search).toLowerCase()) >= 0)
+              .map(user => (
+                <View key={user.username} style={{ flexDirection: 'row', alignItems: 'center', borderTopWidth: 1, borderTopColor: '#e5e5e5', padding: 10 }}>
+                  <Image source={{ uri: `${user.profileImageUrl}` }} style={{ height: 55, width: 55, borderRadius: 55 / 2, marginRight: 12 }} /> 
+                  <Text style={{ fontWeight: 'bold' }}>{user.username}</Text>
                 </View>
-              ))}
+            ))}
           </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
