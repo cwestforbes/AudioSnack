67fdf56443311326ce4ae9999fc4eccea3fe4fe5
Cory West-Forbes <cwestforbes@gmail.com>
2018-07-24 04:18:22 -0700

    alter dimensions

--- src/app/Record.js
+++ src/app/Record.js
@@ -165,6 +165,7 @@ export class Record extends Component {
       console.log('ERROR UNLOADING ASYNC')
     }
     const audioFile = await FileSystem.getInfoAsync(this.recording.getURI());
+    alert(audioFile.uri);
 
     await this.setState({
       recordingUri: audioFile.uri
@@ -240,11 +241,11 @@ export class Record extends Component {
       return (
         <View>
           <View>
-            <Text style={{ marginTop: 60, textAlign: 'center', fontSize: 62, fontWeight: '200', marginBottom: 100}}>{convertDurationToStr(timer)}
+            <Text style={{ marginTop: 60, textAlign: 'center', fontSize: 62, fontWeight: '200', marginBottom: 60}}>{convertDurationToStr(timer)}
             </Text>
           </View>
           <Image source={require('./../../public/img/hold-waveform.png')} style={{ height: 200, width: 380 }} />
-          <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 50, marginBottom: 30 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 30, marginBottom: 30 }}>
             <Image source={require('./../../public/img/cancelBtn.png')} style={{ height: 70, width: 70 }} />
             {!this.state.isRecording ? (
               <TouchableOpacity
