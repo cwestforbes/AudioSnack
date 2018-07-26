811ffd51062b6ba798bc56b7402d3f97c3a8dcc7
Cory West-Forbes <cwestforbes@gmail.com>
2018-07-26 08:24:44 -0700

    remove typo

--- src/app/ProfileView.js
+++ src/app/ProfileView.js
@@ -125,7 +125,7 @@ export class ProfileView extends Component {
       this.setState({
         isFollowing: true
       })
-    } else {-
+    } else {
       if (ref === undefined) {
         console.log('error')
       }
